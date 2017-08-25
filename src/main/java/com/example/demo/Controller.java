fdf3ed613215af1d9cbf9d3acfb3b830b7450859
mahderf <30417140+mahderf@users.noreply.github.com>
2017-08-24 23:58:40 -0400

    theme test

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/")
+    public String test()
+    {
+        return"index";
+    }
+}
+
